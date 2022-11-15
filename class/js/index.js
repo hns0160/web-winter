06506d23c3d6e0e57ad79deb6c44021cf04a497d
hns0160 <117334069+hns0160@users.noreply.github.com>
2022-11-15 15:32:30 -0800

    hw 1 JS

---
+++ class/js/index.js
@@ -0,0 +1,32 @@
+
+function adder (a,b){
+    return a + b
+    }
+
+function dividerr(e,f){
+    d = e / f;
+ 
+    return d
+    }
+
+function celsius(f){
+    let a = (f -32);
+    let c = (a*5)/9;
+    return c
+}
+
+function sLength(a){
+    return a.length
+    }
+
+function concat(a,b){
+    return a + b
+    }
+
+
+console.log(adder(5,8),"add")
+console.log(dividerr(18/9),"divider")
+console.log(celsius(15),"convert to celsius")
+console.log(sLength("abcde", "string length"))
+console.log(concat("ball","game"),"concatenation")
+console.log(9 / 3)
